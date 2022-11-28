86a4f7ce2a6025897741a0bb5de511e8da384de0
253756 <m253756@usna.edu>
2022-11-28 14:19:46 -0500

    adding obstacle and movement

--- Final.py
+++ Final.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from ship_1 import Ship1
+from ship_2 import Ship2
 
 pygame.init()
 
@@ -12,16 +13,24 @@ screen.fill((0,0,0))
 screen_rect = screen.get_rect()
 rows = screen_rect.height // tile_size
 cols = screen_rect.width // tile_size
-#drwaing my ocean onthe screen
-for x in range(int(rows)):
-    for y in range(int(cols)):
-        screen.blit(background_tile, (x*water_rect.height, y*water_rect.width))
+
+#drawing my ocean onthe screen
+def update():
+    for x in range(int(rows)):
+        for y in range(int(cols)):
+            screen.blit(background_tile, (x*water_rect.height, y*water_rect.width))
+
 #bring in player 1
 player1 = Ship1()
 
 pygame.display.flip()
+#points for the first ship origin
+x=50
+y=520
 
-def left_move(self):
+#first obstacle
+obstacle = Ship2()
+position_y = 0
 
 
 while True:
@@ -29,7 +38,15 @@ while True:
         if event.type ==pygame.QUIT:
             pygame.quit()
             sys.exit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x-=3
+    elif keys[pygame.K_RIGHT]:
+        x+=3
+    position_y += 1
+    update()
     player1_rect = player1.rect
-    player1.draw(screen)
-    screen.blit(player1.image, player1_rect)
+    #player1.draw(screen)
+    screen.blit(player1.image, (x,y))
+    screen.blit(obstacle.image,(0,position_y))
     pygame.display.flip()
--- ship_1.py
+++ ship_1.py
@@ -1,23 +1,53 @@
 import pygame
+import sys
 
 class Ship1:
     def __init__(self):
         self.image = pygame.image.load("images/player_1.png")
         self.rect = self.image.get_rect()
-        #movement flags
+        # movement flags
         self.moving_right = False
         self.moving_left = False
 
-    def update(self):
-        """Update the ship's position based on teh movement flag"""
-        # Update the ship's x value, not the rect.
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
-        if self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.ship_speed
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self,event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.moving_right = True
+            print("no")
+        elif event.key == pygame.K_LEFT:
+            self.moving_left = True
+            print('n')
+
+    def _check_keyup_events(self,event):
+        """"Respond to key realeases."""
+        if event.key == pygame.K_RIGHT:
+            self.moving_right = False
+            print("yes")
+        elif event.key == pygame.K_LEFT:
+            self.moving_left = False
+            print("y")
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+   # def update(self):
+    #    """Update the ship's position based on teh movement flag"""
+     #   # Update the ship's x value, not the rect.
+      #  if self.moving_right and self.rect.right < self.screen_rect.right:
+       #     self.x += self.settings.ship_speed
+       # if self.moving_left and self.rect.left > 0:
+        #    self.x -= self.settings.ship_speed
 
         # update rect object from self.x.
-        self.rect.x = self.x
-  
+      #  self.rect.x = self.x
     def draw(self, surface):
         surface.blit(self.image, self.rect)
